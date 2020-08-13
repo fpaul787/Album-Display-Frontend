dedd94ca8108c6bf4d3f537a86c88e69b3093e38
Frantz Paul <fpaul787@gmail.com>
2020-08-13 01:10:42 -0400

    Updates

---
+++ precache-manifest.1ca12eac28f03a0d630e789014b6d282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397a6221a88109b3c708c0fdd5ae5fb8",
+    "url": "/Album-Display-Frontend/index.html"
+  },
+  {
+    "revision": "ead874aa3cd2dc00be42",
+    "url": "/Album-Display-Frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5e92a7383ca03953993e",
+    "url": "/Album-Display-Frontend/static/css/main.e326b7e3.chunk.css"
+  },
+  {
+    "revision": "ead874aa3cd2dc00be42",
+    "url": "/Album-Display-Frontend/static/js/2.8acbd9a6.chunk.js"
+  },
+  {
+    "revision": "1a17c67f4311437e33b522bcde67da07",
+    "url": "/Album-Display-Frontend/static/js/2.8acbd9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e92a7383ca03953993e",
+    "url": "/Album-Display-Frontend/static/js/main.53ad110a.chunk.js"
+  },
+  {
+    "revision": "dc4e5d76af91de918305",
+    "url": "/Album-Display-Frontend/static/js/runtime-main.4b287c87.js"
+  }
+]);
